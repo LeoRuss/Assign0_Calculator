4d6ec3bc5031df9ec1dd1ab22cf0b47d9a56e9f2
Russel Rai <leon.amorphy@googlemail.com>
2020-11-02 16:59:04 +0000

    added contributors and comments

--- App.java
+++ App.java
@@ -1,5 +1,7 @@
 /*
-*
+*This is Assignment 0: Formative task of making a calculator appp for CS1701 Group Project.
+* Main contruibutors for this projects and huge thanks for everyone for contributing in this project to make it happen. Allham, Casper, Hamza, Miguel, Russel and Taron.
+* 
 */
 
 public class App {
