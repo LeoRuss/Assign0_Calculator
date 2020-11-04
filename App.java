62a9e75d3d638f1a34591a98d6c12bd4a3752e7c
Russel Rai <leon.amorphy@googlemail.com>
2020-11-04 14:33:28 +0000

    changed comment

--- App.java
+++ App.java
@@ -1,11 +1,17 @@
+import java.util.ArrayList;
 /*
 *This is Assignment 0: Formative task of making a calculator appp for CS1701 Group Project.
-* Main contruibutors for this projects and huge thanks for everyone for contributing in this project to make it happen. Allham, Casper, Hamza, Miguel, Russel and Taron.
+* Main contruibutors for this projects and huge thanks for  Allham, Casper, Hamza, Miguel, Russel and Taron contributing in this project to make it happen.
 * 
 */
 
 public class App {
   public static void main(String[] args) {
+    String story = "\t\"How can you stand it?\" he asked sarcastically.\n";
+    System.out.println(story);
+
+    String[] arrTest = new String[5];
+    ArrayList<String> arrTest2 = new ArrayList<String>();
 
   }
 }
